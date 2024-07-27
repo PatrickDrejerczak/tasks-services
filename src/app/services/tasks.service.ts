0820434b4011b132f812f6b596a4da5c5f48ccbc
PatrickDrejerczak <drejerczak@aol.com>
2024-07-27 16:20:45 +0200

    finished tasks service, added filter, added status chang

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from '../tasks/task.model';
+import { Task, TaskStatus } from '../tasks/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+
+  allTasks = this.tasks.asReadonly();
 
   addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
@@ -12,5 +14,13 @@ export class TasksService {
       status: 'OPEN',
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
   }
 }
